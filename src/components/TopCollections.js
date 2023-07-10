d73481e2c4f92171caaa8b330fbccd04218fb44f
funkiirabu <phil.vadala@optus.com.au>
2023-07-10 18:30:13 +1000

    Adding data to table

--- src/components/TopCollections.js
+++ src/components/TopCollections.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 
-// Define your query
 const FETCH_TOP_COLLECTIONS = gql`
   query fetchTopCollections($offset: Int, $limit: Int) {
     sui {
@@ -25,29 +24,48 @@ const FETCH_TOP_COLLECTIONS = gql`
   }
 `;
 
-// Create your component
 const TopCollections = () => {
-  // Make the query
   const { loading, error, data } = useQuery(FETCH_TOP_COLLECTIONS, {
     variables: { limit: 10 },
   });
 
-  // Handle the different states
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  // Once the data is loaded, map over it and display it
-  return data.sui.collections.map((collection) => (
-    <div key={collection.id}>
-      <h2>{collection.title}</h2>
-      <img src={collection.cover_url} alt={collection.title} className="w-64 h-64 object-cover" />
-      <p>Supply: {collection.supply}</p>
-      <p>USD Volume: {collection.usd_volume}</p>
-      <p>Volume: {collection.volume}</p>
-      <p>Floor: {collection.floor}</p>
-      <p>Verified: {collection.verified ? 'Yes' : 'No'}</p>
-    </div>
-  ));
+  return (
+    <table className="table-auto w-full mt-10">
+      <thead>
+        <tr>
+          <th>Cover</th>
+          <th>Title</th>
+          <th>Supply</th>
+          <th>USD Volume</th>
+          <th>Volume</th>
+          <th>Floor</th>
+          <th>Verified</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.sui.collections.map((collection) => (
+          <tr key={collection.id}>
+            <td>
+              <img
+                className="w-20 h-20 object-cover"
+                src={collection.cover_url}
+                alt={collection.title}
+              />
+            </td>
+            <td>{collection.title}</td>
+            <td>{collection.supply}</td>
+            <td>{collection.usd_volume}</td>
+            <td>{collection.volume}</td>
+            <td>{collection.floor}</td>
+            <td>{collection.verified ? 'Yes' : 'No'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default TopCollections;
