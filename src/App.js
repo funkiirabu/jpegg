a0313c83f83258c1d72c9de288629a3d77cb85f7
funkiirabu <phil.vadala@optus.com.au>
2023-07-10 18:29:04 +1000

    Rendering TopCollections in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // App.js
 import React from 'react';
-import FetchCollection from './components/FetchCollection'; // FetchCollection.js
+// import FetchCollection from './components/FetchCollection'; // FetchCollection.js
+import TopCollections from './components/TopCollections'; // TopCollections.js
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className="text-center mt-20">
       <h1 className="text-5xl">Project Jpegg</h1>
-        <FetchCollection slug="0x71ef69d02e77dff830c7de41db1452928c8ecd9040a541eef6729f139df83ffd" />
+      {/* <FetchCollection slug="0x71ef69d02e77dff830c7de41db1452928c8ecd9040a541eef6729f139df83ffd" /> */}
+      <TopCollections />
     </div>
   );
 }
