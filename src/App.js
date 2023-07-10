7def3b392abe98945ae97e58c2f212f6a241882f
funkiirabu <phil.vadala@optus.com.au>
2023-07-10 12:10:38 +1000

    Adding some basic styling to FetchCollection component

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <FetchCollection slug="0x71ef69d02e77dff830c7de41db1452928c8ecd9040a541eef6729f139df83ffd" />
+    <div className="text-center mt-20">
+      <h1 className="text-5xl">Project Jpegg</h1>
+        <FetchCollection slug="0x71ef69d02e77dff830c7de41db1452928c8ecd9040a541eef6729f139df83ffd" />
     </div>
   );
 }
--- src/components/FetchCollection.js
+++ src/components/FetchCollection.js
@@ -1,6 +1,7 @@
 // src/components/FetchCollection.js
 import React from 'react';
 import { useQuery, gql } from '@apollo/client';
+import '../App.css';  // Make sure this is the correct path to your CSS file
 
 // Define your query
 const FETCH_COLLECTION_INFO = gql`
@@ -30,13 +31,17 @@ const FetchCollection = ({ slug }) => {
 
   // Once the data is loaded, map over it and display it
   return data.sui.collections.map((collection) => (
-    <div key={collection.slug}>
+    <div key={collection.slug} className="flex flex-col items-center">
       <h2>{collection.title}</h2>
-      <img src={collection.cover_url} alt={collection.title} />
+      <img
+        src={collection.cover_url}
+        alt={collection.title}
+        className="w-64 h-64 object-cover"
+      />
       <p>Supply: {collection.supply}</p>
       <p>Verified: {collection.verified ? 'Yes' : 'No'}</p>
     </div>
-  ));
+  ));  
 };
 
 export default FetchCollection;
