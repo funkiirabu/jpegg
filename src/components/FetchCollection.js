1fdbc387dbb314110a02c3f01d796a274c47bf2d
funkiirabu <phil.vadala@optus.com.au>
2023-07-10 10:02:44 +1000

    Creating component to fetch data using the useQuery hook

---
+++ src/components/FetchCollection.js
@@ -0,0 +1,42 @@
+// src/components/FetchCollection.js
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+// Define your query
+const FETCH_COLLECTION_INFO = gql`
+  query fetchCollectionInfo($slug: String!) {
+    sui {
+      collections(where: { slug: { _eq: $slug } }) {
+        slug
+        title
+        cover_url
+        supply
+        verified
+      }
+    }
+  }
+`;
+
+// Create your component
+const FetchCollection = ({ slug }) => {
+  // Make the query
+  const { loading, error, data } = useQuery(FETCH_COLLECTION_INFO, {
+    variables: { slug },
+  });
+
+  // Handle the different states
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  // Once the data is loaded, map over it and display it
+  return data.sui.collections.map((collection) => (
+    <div key={collection.slug}>
+      <h2>{collection.title}</h2>
+      <img src={collection.cover_url} alt={collection.title} />
+      <p>Supply: {collection.supply}</p>
+      <p>Verified: {collection.verified ? 'Yes' : 'No'}</p>
+    </div>
+  ));
+};
+
+export default FetchCollection;
