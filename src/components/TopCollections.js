be7ecbb27b326ef4245ce6ce6937a99a5048e126
Phil Vadala <phil.vadala@optus.com.au>
2023-07-11 21:43:55 +1000

    Adding Dropdown element and 7 day filter

--- src/components/TopCollections.js
+++ src/components/TopCollections.js
@@ -1,8 +1,6 @@
-// src/components/TopCollections.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery, gql } from '@apollo/client';
 
-// GraphQL query to fetch trending collections
 const FETCH_TRENDING_COLLECTIONS = gql`
   query fetchTrendingCollections(
     $period: TrendingPeriod!
@@ -40,31 +38,30 @@ const FETCH_TRENDING_COLLECTIONS = gql`
 `;
 
 const TopCollections = () => {
-  // Fetch data using the GraphQL query
+  const [period, setPeriod] = useState('days_1');
+
   const { loading, error, data } = useQuery(FETCH_TRENDING_COLLECTIONS, {
     variables: {
-      period: "days_1",
-      trending_by: "usd_volume",
+      period,
+      trending_by: 'usd_volume',
       offset: 0,
       limit: 10
     }
   });
 
-  // Loading state: Display a loading message while data is being fetched
-  if (loading) return <p>Loading...</p>;
+  const handlePeriodChange = (event) => {
+    setPeriod(event.target.value);
+  };
 
-  // Error state: Display an error message if there's an error fetching the data
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  // Function to format a number as USD currency
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
   };
 
-  // Function to compute percentage change
   const computePercentageChange = (current, previous) => ((current - previous) / previous) * 100;
 
-  // Function to render percentage change with appropriate styling
   const renderChange = (current, previous) => {
     const percentageChange = computePercentageChange(current, previous);
     return (
@@ -78,50 +75,67 @@ const TopCollections = () => {
   };
 
   return (
-    <table className="table-auto w-full mt-10">
-      <thead>
-        <tr>
-          <th>Cover</th>
-          <th>Title</th>
-          <th>Sales</th>
-          <th>24h USD Volume</th>
-          <th>24h Sui Volume</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.sui.collections_trending.map((trendingCollection) => (
-          <tr key={trendingCollection.id}>
-            <td>
-              {/* Display the collection cover image */}
-              <img
-                className="w-20 h-20 object-cover"
-                src={trendingCollection.collection.cover_url}
-                alt={trendingCollection.collection.title}
-              />
-            </td>
-            <td>{trendingCollection.collection.title}</td>
-            <td>
-              {/* Display the current trades count */}
-              {trendingCollection.current_trades_count}
-              {/* Show the percentage change for trades count */}
-              {renderChange(trendingCollection.current_trades_count, trendingCollection.previous_trades_count)}
-            </td>
-            <td>
-              {/* Show the current USD volume, formatted as currency */}
-              {formatCurrency(trendingCollection.current_usd_volume)}
-              {/* Show the percentage change for USD volume */}
-              {renderChange(trendingCollection.current_usd_volume, trendingCollection.previous_usd_volume)}
-            </td>
-            <td>
-              {/* Display the current volume */}
-              {trendingCollection.current_volume}
-              {/* Show the percentage change for volume */}
-              {renderChange(trendingCollection.current_volume, trendingCollection.previous_volume)}
-            </td>
+    <div>
+      <label htmlFor="period" className="mr-2">
+        Select Period:
+      </label>
+      <select
+        id="period"
+        value={period}
+        onChange={handlePeriodChange}
+        className="p-2 rounded"
+      >
+        <option value="days_1">1 Day</option>
+        <option value="days_7">7 Days</option>
+      </select>
+
+      <table className="table-auto w-full mt-10">
+        <thead>
+          <tr>
+            <th>Cover</th>
+            <th>Title</th>
+            <th>Sales</th>
+            <th>24h USD Volume</th>
+            <th>24h Sui Volume</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.sui.collections_trending.map((trendingCollection) => (
+            <tr key={trendingCollection.id}>
+              <td>
+                <img
+                  className="w-20 h-20 object-cover"
+                  src={trendingCollection.collection.cover_url}
+                  alt={trendingCollection.collection.title}
+                />
+              </td>
+              <td>{trendingCollection.collection.title}</td>
+              <td>
+                {trendingCollection.current_trades_count}
+                {renderChange(
+                  trendingCollection.current_trades_count,
+                  trendingCollection.previous_trades_count
+                )}
+              </td>
+              <td>
+                {formatCurrency(trendingCollection.current_usd_volume)}
+                {renderChange(
+                  trendingCollection.current_usd_volume,
+                  trendingCollection.previous_usd_volume
+                )}
+              </td>
+              <td>
+                {trendingCollection.current_volume}
+                {renderChange(
+                  trendingCollection.current_volume,
+                  trendingCollection.previous_volume
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
