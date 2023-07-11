9eb00658777abddc1c8fa9e46805a3329e5c113c
funkiirabu <phil.vadala@optus.com.au>
2023-07-11 15:20:17 +1000

    Adding SSR to Apollo Client

--- src/apollo.js
+++ src/apollo.js
@@ -1,19 +1,21 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { getDataFromTree } from '@apollo/client/react/ssr';
 
-const apiUrl = process.env.REACT_APP_INDEXER_API_URL;
-const apiKey = process.env.REACT_APP_API_KEY;
-const apiUser = process.env.REACT_APP_API_USER;
-
-// Create an http link:
+// Create an http link for SSR
 const httpLink = createHttpLink({
-  uri: apiUrl,
+  uri: process.env.REACT_APP_INDEXER_API_URL,
   headers: {
-    'x-api-key': apiKey,
-    'x-api-user': apiUser,
+    'x-api-key': process.env.REACT_APP_API_KEY,
+    'x-api-user': process.env.REACT_APP_API_USER,
   }
 });
 
-export const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+// Create an instance of ApolloClient with the SSR link and cache
+export const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: true, // Enable SSR mode
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+};
+
